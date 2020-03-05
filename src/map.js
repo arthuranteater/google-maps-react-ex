52663f57d0d4ac7c41b0ad055bf3ab041481576d
Hunt Applegate <huntapplegate@gmail.com>
2020-03-05 01:02:47 -0700

    current loc

--- src/map.js
+++ src/map.js
@@ -3,34 +3,38 @@ import React from 'react'
  
  
 export class MapContainer extends React.Component {
-    state = {
-        x: 40.854885,
-        y: -88.081807,
-    }
+    state = {}
 
     componentDidMount() {
         console.log('mounted')
-        
-    }
+         if (window.navigator.geolocation) {
+             console.log('setting coords')
+             window.navigator.geolocation.getCurrentPosition(c => this.setState({x: c.coords.latitude, y: c.coords.longitude}))
+            }
+        }
 
     render() {
+        
+        const {x, y} = this.state
+
         return (
+            <>
+            {Object.keys(this.state).length !== 0 ?
          <Map
           google={this.props.google}
           initialCenter={{
-            lat: this.state.x,
-            lng: this.state.y
+            lat: x,
+            lng: y
           }}
           zoom={8}
           onClick={this.onMapClicked}
         >
-          </Map>
+          </Map> :
+          <></>
+            }
+            </>
         )
     }
-};
-
-const key = process.env.REACT_APP_TUT_MAP
-
-console.log(key)
+}
 
 export default GoogleApiWrapper({apiKey: process.env.REACT_APP_TUT_MAP})(MapContainer)
